9b9d82a551bee0940eb77a3dfa499f712ace6a2b
sixtis1 <asdw2001@gmail.com>
2024-10-09 13:30:49 +0600

    feat: added docker config with postgres db

---
+++ src/configuration/index.ts
@@ -0,0 +1,8 @@
+export default () => ({
+    port: process.env.PORT || 3000,
+    db_port: process.env.DB_PORT,
+    db_name: process.env.DB_NAME,
+    db_user: process.env.DB_USERNAME,
+    db_password: process.env.DB_PASSWORD,
+    db_host: process.env.DB_HOST
+})
